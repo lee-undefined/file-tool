9a32b48bbeaa56351a681adc796cae141577bfde
undefined <515023802@qq.com>
2022-05-02 17:07:10 +0800

    chore: antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+import { DatePicker, Button } from "antd";
 
 function openFile() {
   window.electronAPI.openFile();
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <Button type="primary" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
-          </button>
-          <button type="button" onClick={openFile}>
+          </Button>
+          <Button type="primary" onClick={openFile}>
             openFile
-          </button>
+          </Button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <DatePicker />
       </header>
     </div>
   );
 }
 
-export default App
+export default App;
